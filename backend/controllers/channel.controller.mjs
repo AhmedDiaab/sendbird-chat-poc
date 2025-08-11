05f9c796798f17b8ada4d6e8395666027bbf225c
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2025-08-11 18:06:05 +0300

    fix(): fix import

--- backend/controllers/channel.controller.mjs
+++ backend/controllers/channel.controller.mjs
@@ -1,4 +1,4 @@
-import { createChannel, joinChannel, leaveChannel, listChannelMembers, listChannels, removeChannel, updateChannel, viewChannel } from '../services/sendbird.service.mjs';
+import { createChannel, inviteToChannel, joinChannel, leaveChannel, listChannelMembers, listChannels, removeChannel, updateChannel, viewChannel } from '../services/sendbird.service.mjs';
 import { chunkedExecute } from '../utils/withChunkExecution.util.mjs';
 
 /* CREATE ----------------------------------------------------------- */
